b83c376ab2fb31e75881665826cf0800eb5cd881
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2019-09-09 15:21:19 +0100

    ACTIVITI-3760 Add spring project module

---
+++ activiti-spring-project/src/main/java/org/activiti/core/common/spring/project/ProjectModelService.java
@@ -0,0 +1,59 @@
+package org.activiti.core.common.spring.project;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.NoSuchFileException;
+import java.util.Optional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.activiti.core.common.project.model.ProjectModel;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+public class ProjectModelService {
+
+
+    private String path;
+
+    private String appName;
+
+    private final ObjectMapper objectMapper;
+
+    private ResourcePatternResolver resourceLoader;
+
+    public ProjectModelService(String path,
+                               String appName,
+                               ObjectMapper objectMapper,
+                               ResourcePatternResolver resourceLoader) {
+        this.path = path;
+        this.appName = appName;
+        this.objectMapper = objectMapper;
+        this.resourceLoader = resourceLoader;
+    }
+
+
+    private Optional<Resource> retrieveResource(){
+
+        Resource resource = resourceLoader.getResource(path);
+        if (resource.exists()) {
+            return Optional.of(resourceLoader.getResource(path + appName + ".json"));
+        }else{
+            return Optional.empty();
+        }
+    }
+
+    private ProjectModel read (InputStream inputStream) throws IOException {
+        return objectMapper.readValue(inputStream,
+                                      ProjectModel.class);
+    }
+
+    public ProjectModel getProjectModel() throws IOException  {
+        Optional<Resource> resourceOptional = retrieveResource();
+        if(resourceOptional.isPresent()){
+            return read(resourceOptional.get().getInputStream());
+        }else{
+            throw new NoSuchFileException(appName + ".json");
+        }
+    }
+
+}
---
+++ activiti-spring-project/src/main/java/org/activiti/core/common/spring/project/autoconfigure/ProjectModelConfiguration.java
@@ -0,0 +1,34 @@
+package org.activiti.core.common.spring.project.autoconfigure;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.activiti.core.common.spring.project.ProjectModelService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+@Configuration
+public class ProjectModelConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnMissingClass(value = "org.springframework.http.converter.json.Jackson2ObjectMapperBuilder")
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public ProjectModelService projectModelService (@Value("") String path, @Value("") String appName, ObjectMapper objectMapper, ResourcePatternResolver resourceLoader){
+        return new ProjectModelService(path, appName, objectMapper, resourceLoader);
+    }
+
+    @Bean
+    public String projectReleaseVersion(ProjectModelService projectModelService) throws IOException {
+        return projectModelService.getProjectModel().getVersion();
+    }
+
+}
