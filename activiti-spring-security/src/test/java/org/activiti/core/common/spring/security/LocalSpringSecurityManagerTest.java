8a84edaa8e14babd5058a8396b8f527cde5b1fdc
Igor Dianov <igor@dianov.com>
2019-10-22 19:28:22 -0700

    feat(test): Added LocalSpringSecurityManagerTest

--- activiti-spring-security/src/test/java/org/activiti/core/common/spring/security/LocalSpringSecurityManagerTest.java
+++ activiti-spring-security/src/test/java/org/activiti/core/common/spring/security/LocalSpringSecurityManagerTest.java
@@ -17,6 +17,7 @@
 package org.activiti.core.common.spring.security;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 import org.activiti.api.runtime.shared.security.SecurityManager;
 import org.junit.Test;
@@ -25,8 +26,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 
 @RunWith(SpringRunner.class)
@@ -39,14 +45,94 @@ public class LocalSpringSecurityManagerTest {
     @Autowired
     private SecurityManager securityManager;
     
-    @Test
-    public void contextLoads() {
-        assertThat(securityManager).isInstanceOf(LocalSpringSecurityManager.class);
-    }
-    
     @SpringBootApplication
     static class Application {
         
     }
+    
+    @Test
+    public void contextLoads() {
+        assertThat(securityManager).isInstanceOf(LocalSpringSecurityManager.class);
+    }
 
+    @Test
+    @WithMockUser(username = "hruser", authorities = {"ROLE_user", "GROUP_users"})
+    public void testGetAuthenticatedUserId() {
+        
+        // when
+        String result = securityManager.getAuthenticatedUserId();
+        
+        // then
+        assertThat(result).isEqualTo("hruser");
+    }
+
+    @Test
+    @WithMockUser(username = "hruser", authorities = {"ROLE_user", "GROUP_users"})
+    public void testGetAuthenticatedUserGroups() {
+        
+        // when
+        List<String> result = securityManager.getAuthenticatedUserGroups();
+        
+        // then
+        assertThat(result).containsExactly("users");
+    }
+
+    @Test
+    @WithMockUser(username = "hruser", authorities = {"ROLE_user", "GROUP_users"})
+    public void testGetAuthenticatedUserRoles() {
+        
+        // when
+        List<String> result = securityManager.getAuthenticatedUserRoles();
+        
+        // then
+        assertThat(result).containsExactly("user");
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void testGetAuthenticatedUserIdAnonymous() {
+        
+        // when
+        String result = securityManager.getAuthenticatedUserId();
+        
+        // then
+        assertThat(result).isEqualTo("anonymous");
+    }
+    
+    @Test
+    public void testGetAuthenticatedUserIdInvalidUser() {
+        // given
+        SecurityContextHolder.clearContext();
+        
+        // when
+        Throwable result = catchThrowable(() -> securityManager.getAuthenticatedUserId());
+        
+        // then
+        assertThat(result).isInstanceOf(SecurityException.class);
+    }
+
+    @Test
+    public void testGetAuthenticatedUserGroupInvalidUser() {
+        // given
+        SecurityContextHolder.clearContext();
+        
+        // when
+        Throwable result = catchThrowable(() -> securityManager.getAuthenticatedUserGroups());
+        
+        // then
+        assertThat(result).isInstanceOf(SecurityException.class);
+    }
+    
+    @Test
+    public void testGetAuthenticatedUserRolesInvalidUser() {
+        // given
+        SecurityContextHolder.clearContext();
+        
+        // when
+        Throwable result = catchThrowable(() -> securityManager.getAuthenticatedUserRoles());
+        
+        // then
+        assertThat(result).isInstanceOf(SecurityException.class);
+    }    
+    
 }
